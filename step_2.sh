4afc6aed7685acad8dcb1c02c03a86dfd1c42e2f
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2023-02-26 23:52:11 +0800

    up

--- step_2.sh
+++ step_2.sh
@@ -13,6 +13,11 @@ COMMENT
   eval "${cmd}"
 }
 
+cd $HOME
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "finish download powerlevel10k"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "finish download zsh-autosuggestions"
 
