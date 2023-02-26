db33dc82772d74df109c0dd688efc5611c2ecb98
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2023-02-27 00:16:45 +0800

    Update add_fonts.sh

--- add_fonts.sh
+++ add_fonts.sh
@@ -24,7 +24,7 @@ use_sudo mv -f MesloLGS_NF /usr/share/fonts/truetype/
 use_sudo fc-cache -f -v
 clear
 use_sudo apt install -y gconf2
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "MesloLGS NF Regular"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "MesloLGS NF"
 echo "finish added fonts"
 
 use_sudo apt install -y git wget curl wget zsh
