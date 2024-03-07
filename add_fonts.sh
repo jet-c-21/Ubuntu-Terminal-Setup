02f4f0f1ee66465123eba6d069bec45ab6306266
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2024-03-07 12:01:47 +0800

    upload

--- add_fonts.sh
+++ add_fonts.sh
@@ -18,9 +18,9 @@ COMMENT
 
 sudo apt install -y fonts-noto-color-emoji
 sudo rm -rf /usr/share/fonts/truetype/MesloLGS_NF
-wget -O MesloLGS_NF.zip https://github.com/jet-c-21/MyFonts/releases/download/1.0.0/MesloLGS_NF.zip
+#wget -O MesloLGS_NF.zip https://github.com/jet-c-21/MyFonts/releases/download/1.0.0/MesloLGS_NF.zip
 unzip -q MesloLGS_NF.zip
-rm -rf MesloLGS_NF.zip
+#rm -rf MesloLGS_NF.zip
 sudo mv -f MesloLGS_NF /usr/share/fonts/truetype/
 sudo fc-cache -f -v
 clear
