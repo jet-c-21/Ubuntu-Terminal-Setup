e1dc466928ecf033b9497846314f8189b76ca2b4
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2024-05-24 12:49:19 +0800

    Update quick_install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -95,7 +95,7 @@ install_powerlevel10k(){
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   unlock_sudo
-  git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+  sudo git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
   echo "finish cloning all zsh plugins"
 
   # Update .zshrc
