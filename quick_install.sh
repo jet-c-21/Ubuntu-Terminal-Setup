3f8f0d1fcb1c34151ef3fbd7f75d9143f80a5786
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2024-05-24 12:46:35 +0800

    update

--- quick_install.sh
+++ quick_install.sh
@@ -95,6 +95,7 @@ install_powerlevel10k(){
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+  echo "finish cloning all zsh plugins"
 
   # Update .zshrc
   sed -i "/^ZSH_THEME=/c\ZSH_THEME=\"powerlevel10k/powerlevel10k\"" ~/.zshrc
