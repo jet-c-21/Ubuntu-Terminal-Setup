714b5bf1e27369a439c304d9f833b91ff6ab28d2
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2024-05-24 12:47:49 +0800

    Update quick_install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -94,6 +94,7 @@ install_powerlevel10k(){
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  unlock_sudo
   git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
   echo "finish cloning all zsh plugins"
 
